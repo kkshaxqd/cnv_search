69dfb50713c60c56bae536137c34e2818722ee0f
Josh Zhang <514079685@qq.com>
2018-01-10 15:50:03 +0800

    Add files via upload

--- server.R
+++ server.R
@@ -1,80 +1,167 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+#server.R
 
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+library(lattice)
+source("src/cnvgram.R")
+library(psych)
+library(ggplot2)
+library(OmicCircos)
+library(rtracklayer)
+options(stringsAsFactors = FALSE);
+shinyServer(function(input,output){
   
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
-  output$distPlot <- renderPlot({
+  dataValues <- reactive({
     
-    # generate an rnorm distribution and plot it
-    dist <- rnorm(input$obs)
-    hist(dist)
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    data<-read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
+    data$count<-data$weight
+    data$count=1
+    
+    return(data)
   })
+  ##DT显示表内容
+  output$table <- DT::renderDataTable(DT::datatable({
+    dataValues() 
+  }, rownames = FALSE))
+  ###cnv画图内容
+  #output$plot1 <- renderPlot({
+    
+    ##data
+    ##psych包里  describe(mtcars[vars]) 统计函数 
+    
+    output$summary<-renderTable({
+      ##要处理准备的数据集
+      if(input$var=="software")
+      {describe(dataValues())}
+      else if(input$var=="gene"){  return(NULL)    }
+      #attach(dataValues())
+      # if(input$var=="software")
+      # {describe((software)) }
+      # else if(input$var=="chr"){describe(chromosome) }
+      # else if(input$var=="TYPE"){describe(TYPE)}
+      # else if(input$var=="gene"){describe(gene)}
+      # else if(input$var=="cn"){describe(cn)}
+      # else if(input$var=="start"){describe(start)}
+      # else if(input$var=="log2"){describe(log2)}
+      # else if(input$var=="end"){describe(end)}
+      # else if(input$var=="SIZE"){describe(SIZE)}
+      # else if(input$var=="probes"){describe(probes)}
+      # else{return(NULL)}
+      #ba<-data.frame(ab)
+     # rownames(ba)<-c("chromosome",	"start",	"end",	"SIZE",	"gene",	"log2",	"cn",	"TYPE",	"depth",	"probes",	"weight",	"software")
+     # return(ba)
+      ##软件变量
+      #z1<-input$softvar
+      #if(z1=="all")
+      #describe(dataValues)
+      #else{
+       # subcnvdata<-cnvdata[cnvdata$software==z1]
+       # describe(subcnvdata$software)
+      #}
+    })
+    output$summaryplot<-renderPlot({
+      #attach(dataValues())
+      if(input$var=="software")
+      {
+        ggplot(dataValues())+geom_bar(aes(x =factor(software),y=count,fill=factor(TYPE)),stat="identity")
+      }
+      else if(input$var=="gene"){
+        ggplot(dataValues())+geom_bar(aes(x =factor(gene),y=count,fill=factor(software)),stat="identity")+theme(axis.text.x = element_text(size = 8,color = "black", face = "bold", vjust = 1, hjust = 1, angle = 60)) 
+      }
+      else if(input$var=="SIZE"){ggplot(dataValues())+geom_bar(aes(x =factor(SIZE),y=count,fill=factor(software)),stat="identity")+theme(axis.text.x = element_text(size = 8,color = "black", face = "bold", vjust = 1, hjust = 1, angle = 60)) 
+      }
+      else if(input$var=="cn"){ggplot(dataValues())+geom_bar(aes(x =factor(cn),y=count,fill=factor(software)),stat="identity")+theme(axis.text.x = element_text(size = 8,color = "black", face = "bold", vjust = 1, hjust = 1, angle = 60)) 
+      }
+      else if(input$var=="TYPE"){ggplot(dataValues())+geom_bar(aes(x =factor(TYPE),y=count,fill=factor(software)),stat="identity")+theme(axis.text.x = element_text(size = 8,color = "black", face = "bold", vjust = 1, hjust = 1, angle = 60)) 
+      }
+      else{return(NULL)}
+      
+    })
+     ###circos图 
+    output$plotcircos <-renderPlot({
+      if(is.null(dataValues()))
+        return(NULL)
+      #cname<-c("ID","chr","po","NAME","cn","color")
+      datanew<-data.frame(dataValues())
+      ID<-seq(1,length(datanew))
+      chr<-datanew$chromosome
+      po<-datanew$start
+      NAME<-datanew$gene
+      #if(dataValues()$log2=="-")
+      #{cn<-dataValues()$cn    }
+      #else{cn<-dataValues()$log2}
+      cn<-datanew$log2
+      color="#33FF0080"
+      #datanew<-data.frame(ID,chr,po,NAME,cn,color)
+      #datanew[datanew$cn=="-"]=3
+      colors <- rainbow(10, alpha=0.5);
+      par(mar=c(2,2,2,2));
+      plot(c(1,800), c(1,800), type="n", axes=FALSE, xlab="", ylab="", main="");
+      circos(R=400, cir="hg19", W=10,   type="chr", lwd=5, side="out", cex=4,print.chr.lab=TRUE, scale=TRUE);
+      circos(R=350, cir="hg19", W=40,  mapping= datanew,  col.v=4,   type="h", B=FALSE, lwd=3, cutoff=0,col=colors [ 4 ] );
+      circos(R=310, cir="hg19", W=40,  mapping= datanew,  col.v=4,   type="s", B=FALSE, lwd=3, cutoff=0,col=colors [ 7 ]);
+      circos(R=250, cir="hg19", W=40,  mapping= datanew,  col.v=4,   type="b3", B=TRUE, lwd=3, cutoff=0,col=colors [ 9 ]);
+      zoom <- c(7, 7, 0, 154143883, 0,360);
+      circos(R=200, cir="hg19", W=40,  mapping= datanew,  col.v=4,   type="s", B=FALSE, lwd=3, cutoff=0,col=colors [ 9 ],zoom=zoom);
+      
+    })
+    #chr1:10000-2000
+    
+    observeEvent(input$submit, {
+      shinyjs::alert("程序正在计算中，速度比较慢，请耐心等待一会，大约一分钟。")
+      region<-input$CNV_reigon
+      #region<-c("chr1:17380437-20915212") #这种格式直接就可以
+      #cnvdata<-gsub("\\:|\\-","    ",region,perl=TRUE)
+      session <- browserSession("UCSC")
+      genome(session) <- "hg19"
+      export(region, "Track12.bed")
+      restoredTrack2 <- import("Track12.bed")
+      track(session, "Track") <-  restoredTrack2
+      browserView(session,restoredTrack2*-5, dense="Track",pack=c("DECIPHER","RefSeq Genes","ClinGen CNVs","ClinVar Variants",
+                         "GeneReviews","OMIM Genes","DGV Struct Var") )
+    })
+    
+      
+      
+      
+      
+    })
+    
+    #hide=c("UCSC Genes","GTEx Gene","ENCODE Regulation...","Conservation","Common SNPs(150)","RepeatMasker"),
+  #output$getdata<- dataValues() 
   
-})
 
-library(shiny)
-library(datasets)
 
-# Define server logic required to summarize and view the selected dataset
-shinyServer(function(input, output) {
-  
-  # By declaring datasetInput as a reactive expression we ensure that:
-  #
-  #  1) It is only called when the inputs it depends on changes
-  #  2) The computation and result are shared by all the callers (it 
-  #     only executes a single time)
-  #  3) When the inputs change and the expression is re-executed, the
-  #     new result is compared to the previous result; if the two are
-  #     identical, then the callers are not notified
-  #
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
-  })
-  
-  # The output$caption is computed based on a reactive expression that
-  # returns input$caption. When the user changes the "caption" field:
-  #
-  #  1) This expression is automatically called to recompute the output 
-  #  2) The new caption is pushed back to the browser for re-display
-  # 
-  # Note that because the data-oriented reactive expression below don't 
-  # depend on input$caption, those expression are NOT called when 
-  # input$caption changes.
-  output$caption <- renderText({
-    input$caption
-  })
-  
-  # The output$summary depends on the datasetInput reactive expression, 
-  # so will be re-executed whenever datasetInput is re-executed 
-  # (i.e. whenever the input$dataset changes)
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
-  # The output$view depends on both the databaseInput reactive expression
-  # and input$obs, so will be re-executed whenever input$dataset or 
-  # input$obs is changed. 
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
-  })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+# 利用lattice包中的绘图函数
+#output$splom <- renderPlot({
+#splom(mtcars[c(1, 3:7)], groups = mtcars$cyl,
+#pscales = 0,pch=1:3,col=1:3,
+#varnames = c("Miles\nper\ngallon", "Displacement\n(cu. in.)",
+#            "Gross\nhorsepower", "Rear\naxle\nratio",
+#           "Weight", "1/4 mile\ntime"),
+# key = list(columns = 3, title = "Number of Cylinders",
+# text=list(levels(factor(mtcars$cyl))),
+# points=list(pch=1:3,col=1:3)))
+#})
+
+#output$wireframe <- renderPlot({
+# wireframe(volcano, shade = TRUE,
+#           aspect = c(61/87, 0.4),
+#            light.source = c(10,0,10))
+#})
+
+#set.seed(122)
+#histdata <- rnorm(500)
--- ui.R
+++ ui.R
@@ -1,67 +1,117 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
-
+# ui.R
 library(shiny)
+library(shinydashboard)
+library(DT)
+library(shinyjs)
+dashboardPage(
+  dashboardHeader(title="CNV结果可视化"
+                  ),
+  dashboardSidebar( 
+    #决定标签显示的名称，和变量
+    
+    sidebarMenu(
+      menuItem("上传文件", tabName = "uploadfile", icon = icon("upload")),
+      menuItem("数据表", tabName = "datatable", icon = icon("th")),
+      menuItem("结果统计", tabName = "picture", icon = icon("map")),
+      menuItem("circos图", tabName = "circos", icon = icon("circle-o")),
+      menuItem("UCSC展示", tabName = "ucsc", icon = icon("universal-access"))
+    )
+    
+                   ),
+  dashboardBody(
+    tabItems(
+      #新的第一个标签下内容
+      tabItem(
+          tabName = "uploadfile",
+          h2("  UPload CNVfile"),
+          sidebarPanel(
+          fileInput('file1', '提交整合后三软件CNV结果文件',
+                  accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+          tags$hr(),
+          checkboxInput('header', 'Header', TRUE),
+          radioButtons('sep', 'Separator',
+                     c(Tab='\t',
+                       Comma=',',
+                       Semicolon=';'
+                     ),
+                     selected="Tab"
+                    # inline=TRUE
+          ),
+          radioButtons('quote', 'Quote',
+                     c(None='',
+                       'Double Quote'='"',
+                       'Single Quote'="'"),
+                     selected="None"
+                     #inline=TRUE
+                     )
+          #numericInput("obs", "Number of observations to view:", 10)
+        )
+      ),
+      
+      # First tab content  第一个标签下内容
+      tabItem(tabName = "datatable",
+              fluidRow(
+                h2("  CNV结果表"),
+                DT::dataTableOutput("table")
+                
+              )
+      ),
+      
+      # Second tab content 第二个标签下内容
+      tabItem(tabName = "picture",
+              #box(plotOutput("plot1", height = 250)),
+              #box(
+                #title = "Controls",
+                #sliderInput("slider", "Number of observations:", 1, 100, 50)
+              #)
+              #sidebarPanel(
+                #selectInput("softvar","软件",
+                          #  c("all"="software",
+                         #     "CNVKIT"="CNVKIT",
+                        #      "XHMM"="XHMM",
+                         #     "CONTRA"="CONTRA",
+                         #     "CONTRA-LARGECNV"="CONTRA-LARGECNV"
+                        #    ),
+                        #    selected="all"
+ 
+              #  )
+    #  ),
+      # rownames(ba)<-c("chromosome",	"start",	"end",	"SIZE",	"gene",	"log2",	"cn",	"TYPE",	"depth",	"probes",	"weight",	"software")
+      sidebarPanel(
+        selectInput("var","列统计",
+                    c("software"="software",
+                      "chr"="chromsome",
+                      "start"="start",
+                      "end"="end",
+                      "SIZE"="SIZE",
+                      "gene"="gene",
+                      "log2"="log2",
+                      "cn"="cn",
+                      "TYPE"="TYPE",
+                      "depth"="depth",
+                      "probes"="probes"
+                    ) 
+          )
+      ),
+      tableOutput("summary"),
+      plotOutput("summaryplot",height = "1000px")
+    ),
+    tabItem(tabName = "circos", 
+      h2("整体Circos图展示"),
+      plotOutput('plotcircos',width = "85%", height = "1000px" )
+      
+    ),
+    tabItem(tabName = "ucsc", 
+            h2("UCSC显示CNV具体情况"),
+            textInput("CNV_reigon", "CNV input","chr1:17380437-20915212"),
+            h5("想要显示的cnv输入时使用如上格式"),
+            shinyjs::useShinyjs(),
+            actionButton("submit", "Submit")
+    )
+    #plotOutput("splom"),
+    #plotOutput("wireframe")
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("中文shiny程序"),
-  
-  
-  # Sidebar with a slider input for number of observations
-  sidebarPanel(
-    sliderInput("obs", 
-                "Number of observations:", 
-                min = 0, 
-                max = 1000, 
-                value = 500)
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot")
   )
-))
-
-
-library(shiny)
-
-# Define UI for dataset viewer application
-shinyUI(pageWithSidebar(
   
-  # Application title
-  headerPanel("Reactivity"),
-  
-  # Sidebar with controls to provide a caption, select a dataset, and 
-  # specify the number of observations to view. Note that changes made
-  # to the caption in the textInput control are updated in the output
-  # area immediately as you type
-  sidebarPanel(
-    textInput("caption", "Caption:", "Data Summary"),
-    
-    selectInput("dataset", "Choose a dataset:", 
-                choices = c("rock", "pressure", "cars")),
-    
-    numericInput("obs", "Number of observations to view:", 10)
-  ),
-  
-  
-  # Show the caption, a summary of the dataset and an HTML table with
-  # the requested number of observations
-  mainPanel(
-    h3(textOutput("caption")), 
-    
-    verbatimTextOutput("summary"), 
-    
-    tableOutput("view")
-  )
-))
+)
+)
